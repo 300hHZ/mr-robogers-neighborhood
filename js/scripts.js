e48d457c66679ded1813b5f626a805a9244c7edc
Woo Jin Kim <kimwoojin211@gmail.com>
2021-01-22 11:07:17 -0800

    add basic CSS styling and loading animation. Loading animation fades into main content after a delay.

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,19 @@ function roboger(number)
   let answer = [];
   let numToString;
   const messages = ["Beep!","Boop!", "Won't you be my neighbor?"];
-  for(let i = 0; i <= number; i++)
+  for(let index1 = 0; index1 <= number; i++)
   {
-    numToString = i.toString();
-    for(let j = 3; j > 0; j--)
+    numToString = index1.toString();
+    for(let index2 = 3; index2 > 0; index2--)
     {
-      if(numToString.includes(j.toString()))
+      if(numToString.includes(index2.toString()))
       {
-        answer.push(" " + messages[j-1]);
+        answer.push(" " + messages[index2-1]);
         break;
       }
     }
 
-    if (answer.length < (i+1)) { answer.push(" " + i); }
+    if (answer.length < (index1+1)) { answer.push(" " + index1); }
   }
   return answer;
 
@@ -29,6 +29,8 @@ function roboger(number)
 // User Interface Logic
 
 $(document).ready(function(){
+  $("#loading").delay(3000).fadeOut(1000);
+  $("#main").delay(4000).fadeIn();
   $("#form").submit(function(event){
     event.preventDefault();
     const input = parseInt($("#number").val());
