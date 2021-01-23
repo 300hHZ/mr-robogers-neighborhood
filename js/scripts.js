fb46a5d0abc0c59a2a54f5f40d328d4f6081cb36
Woo Jin Kim <kimwoojin211@gmail.com>
2021-01-22 16:36:00 -0800

    add reverse button (but doesn't work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 /*Wooj self-notes/thoughts:
-Discovered a bug that submission would still work if input contained both numbers and letters. Made use of a RegExp object and .test to fix big.
 */
 
 // Business Logic
@@ -27,15 +26,14 @@ function roboger(number)
 
 function loadingTextAnimation(dot1,dot2,dot3)
 {
-    setInterval(function () {
+    setInterval(function() {
       dot1.hide();
       dot2.hide();
       dot3.hide();
       dot1.delay(300).fadeIn(0);
       dot2.delay(600).fadeIn(0);
       dot3.delay(900).fadeIn(0);
-    }
-    ,1200);
+    }, 1200);
 
 }
 
@@ -44,30 +42,43 @@ function loadingTextAnimation(dot1,dot2,dot3)
 $(document).ready(function(){
   //loading image and text w/ animation
   loadingTextAnimation($(".firstDot"), $(".secondDot"),$(".thirdDot"), 4000);
+
   //transition to main content
   $("#loading").delay(3000).fadeOut(1000);
   $("#main").delay(4000).fadeIn();
+
+  // //determines if regular or reverse
+  // let buttonpressed;
+  // $('#form').click(function (event) {
+  //   buttonpressed = $(this).attr('name');
+  // });
+
   //submit button logic
   $("#form").submit(function(event){
     event.preventDefault();
     const input = $("#number").val();
-    const nonnumber = new RegExp(/\D/); 
+
     //calculation transition
     $(".answer").hide();
     $("#calculating").fadeIn();
     $("#calculating").delay(2000).fadeOut();
-    
-    if (isNaN(parseInt(input)) || nonnumber.test(input) || parseInt(input < 0))
-      {
-        $("#angryRobo").delay(3000).fadeIn();
-        $("#form").delay(3600).fadeOut();
-        setTimeout(function () { alert("Uh oh. You made Mr. Roboger mad. Please enter a non-negative integer."); }, 4000);
-        $("#angryRobo").delay(1000).fadeOut();
-        $("#form").delay(400).fadeIn();
-      }
 
+    //invalid input
+    if (/\D/.test(input) || isNaN(parseInt(input)) || parseInt(input) < 0)
+    {
+      $("#angryRobo").delay(3000).fadeIn();
+      $("#form").delay(3600).fadeOut();
+      setTimeout(function () { alert("Uh oh. You made Mr. Roboger mad. Please enter a non-negative integer."); }, 4000);
+      $("#angryRobo").delay(1000).fadeOut();
+      $("#form").delay(400).fadeIn();
+    }
+
+    //valid input
     else{
-      $(".output").text(roboger(parseInt(input)));
+      if($(this).attr("name")==="reverse")
+        $(".output").text(roboger(parseInt(input)).reverse());
+      else
+        $(".output").text(roboger(parseInt(input)));
       $(".answer").delay(3000).fadeIn();
     }
   });
